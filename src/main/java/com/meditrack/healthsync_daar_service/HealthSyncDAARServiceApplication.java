dd12f04027552cac4e044c6a5a1dc5fcfe2ed21f
Kevin Kularathne <47452568+demnofocus@users.noreply.github.com>
2024-11-27 08:59:22 +0530

    Initial Project

---
+++ src/main/java/com/meditrack/healthsync_daar_service/HealthSyncDAARServiceApplication.java
@@ -0,0 +1,13 @@
+package com.meditrack.healthsync_daar_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthSyncDAARServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthSyncDAARServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meditrack/healthsync_daar_service/HealthSyncDAARServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meditrack.healthsync_daar_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthSyncDAARServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
